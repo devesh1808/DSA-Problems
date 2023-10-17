080f412d7ca2e836e9eaa8cb8122472c2080a390
devesh1808 <devesh1808@gmail.com>
2023-10-17 23:50:58 +0530

    largest laength of subarray that sums to k

---
+++ lengthOfSubarraySumsToK.cpp
@@ -0,0 +1,18 @@
+//return max length subarray
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        mp[0] = 1;
+        int cnt = 0, sum = 0;
+        for(int ele:nums){
+            sum += ele;
+            if(mp.find(sum-k)!=mp.end()){
+                cnt += mp[sum-k];
+            }
+            mp[sum]++;
+        }
+        return cnt;
+    }
+};
