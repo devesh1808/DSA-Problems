de2085afa5f4016632d2a4798a07095e62beb526
devesh1808 <devesh1808@gmail.com>
2023-12-01 22:55:39 +0530

    Minimum One Bit Operations to Make Integers Zero

---
+++ leetcode1611.cpp
@@ -0,0 +1,11 @@
+int minimumOneBitOperations(int n) {
+        int output = 0;
+        
+        while( n> 0)
+        {
+            output ^= n;
+            n = n >> 1;
+        }
+        
+        return output;
+    }
