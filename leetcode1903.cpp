4d3605b0b66ddb70142a98efdae4f65ab1b8071c
devesh1808 <devesh1808@gmail.com>
2023-12-07 11:21:50 +0530

    Largest Odd Number in String

---
+++ leetcode1903.cpp
@@ -0,0 +1,8 @@
+string largestOddNumber(string num) {
+        int n = num.length();
+        for(int i=n-1; i>=0; i--){
+            int dig = num[i]-'0';
+            if(dig%2!=0) return num.substr(0, i+1);
+        }
+        return "";
+}
