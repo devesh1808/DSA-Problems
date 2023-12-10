dc3501d369708d58ba78f4ac6fc8dc7f74dbcde9
devesh1808 <devesh1808@gmail.com>
2023-12-10 22:30:42 +0530

    Transpose Matrix

---
+++ leetcode867.cpp
@@ -0,0 +1,10 @@
+vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size();
+        vector<vector<int>> mat(n, vector<int>(m, 0));
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                mat[i][j] = matrix[j][i];
+            }
+        }
+        return mat;
+    }
