f56a68f3eee9d3109978bc3597fe9d93b2b97f60
devesh1808 <devesh1808@gmail.com>
2024-02-08 17:15:33 +0530

    Perfect Squares

---
+++ leetcode279.cpp
@@ -0,0 +1,14 @@
+int solve(int n, vector<int> &dp) {
+        if (n == 0) return 0;
+        if (dp[n] != -1) return dp[n];
+        int ans = INT_MAX;
+        for(int i=1; i*i<=n; i++) {
+            ans = min(ans, solve(n-(i*i), dp)+1);
+        }
+        return dp[n]=ans;
+    }
+    int numSquares(int n) {
+        vector <int> dp(n+1, -1);
+        int ans = solve(n, dp);
+        return ans;
+    }
